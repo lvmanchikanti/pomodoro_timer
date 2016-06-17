20abfe16cd5379f613843279907a8232f97280e5
Lahari <lahu.manchik@ufl.edu>
2016-06-17 19:39:11 -0400

    set up server and linked html and css

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var server = express();
+
+server.use(express.static(__dirname+'/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.listen(8080, function(){
+  console.log('Now listening for requests');
+}};
